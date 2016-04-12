6da1aa0314f8a4ce531166b98e9b06758a20f8e6
y-z-f <976691141@qq.com>
2016-04-12 09:50:06 +0800

    初始化项目

---
+++ AChatEngine.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AChatEngine.Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sino")]
+[assembly: AssemblyProduct("AChatEngine.Sample")]
+[assembly: AssemblyCopyright("Copyright © Sino 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sino.Droid.AChatEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sino.Droid.AChatEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sino")]
+[assembly: AssemblyProduct("Sino.Droid.AChatEngine")]
+[assembly: AssemblyCopyright("Copyright © Sino 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
